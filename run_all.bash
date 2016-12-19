a044a02c3bb3ef28722802f51b9953d8384bd2ab
Ehud Ben-Reuven <udi@benreuven.com>
2016-12-19 17:04:36 +0200

    rename mnist-jacob-reed since it also runs on cifar100

--- run_all.bash
+++ run_all.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-python mnist-jacob-reed.py --seed 42 "$@" &
-python mnist-jacob-reed.py --seed 43 "$@" &
-python mnist-jacob-reed.py --seed 44 "$@" &
-python mnist-jacob-reed.py --seed 45 "$@" &
-python mnist-jacob-reed.py --seed 46 "$@"
+python jacob-reed.py --seed 42 "$@" &
+python jacob-reed.py --seed 43 "$@" &
+python jacob-reed.py --seed 44 "$@" &
+python jacob-reed.py --seed 45 "$@" &
+python jacob-reed.py --seed 46 "$@"
